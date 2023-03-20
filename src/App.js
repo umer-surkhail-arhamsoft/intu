35afd92f53eeec0c7fd95221ce296f8e2b6148f3
aminaanwaraa <amina.anwer@arhamsoft.com>
2023-03-20 14:42:31 +0500

    vault creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider  } from "react-router-dom";
+import Balances from "./balances";
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Balances/>
+  }
+])
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/balances.jsx
@@ -0,0 +1,47 @@
+import React,{useEffect} from 'react'
+import { ethers } from "ethers";
+import abiData from "./abi.json"
+
+export default function Balances() {
+
+  const INTU_ADDRESS = "0x6c92E8018cF1284986B95A26d5666CA2bB8Aa9D7";
+
+  useEffect(() => {
+    const contract = async() => {
+       let web3Provider = new ethers.BrowserProvider(window.ethereum)
+       const signer = await web3Provider.getSigner();
+
+  
+       const INTUContract = await new ethers.Contract(INTU_ADDRESS, abiData, signer)
+
+
+      // Vault Creation
+      
+      let arrayOfAddresses = [
+        "0x5A2aC68d4517a773747Ed03cB7A04fd0884B7B87",
+        "0x564C37233eDC7F3C11418Fda93aeCcC9ABB3f625"
+      ]
+
+      let rotateThreshold = "100"
+      let txThreshold = "100"
+      let adminThreshold = "100"
+
+      let participantsPublicKeysForVault = [
+        "0x5A2aC68d4517a773747Ed03cB7A04fd0884B7B87",
+        "0x564C37233eDC7F3C11418Fda93aeCcC9ABB3f625"
+      ]
+
+      let vaultMessage = "First Vault"
+
+      INTUContract.create([arrayOfAddresses], "MyFirstVault", [rotateThreshold,txThreshold,adminThreshold], [participantsPublicKeysForVault], [], vaultMessage)
+
+
+      console.log(INTUContract.getUserVaults())
+    }
+    contract()
+  },[])
+
+  return (
+    <div>balances</div>
+  )
+}
